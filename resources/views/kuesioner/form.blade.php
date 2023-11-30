24838352d93e4466d1fab9c838525fb25cca88e6
faisalach <faisalachramsyah@gmail.com>
2023-11-30 19:40:47 +0700

    update form abc

--- resources/views/kuesioner/form.blade.php
+++ resources/views/kuesioner/form.blade.php
@@ -91,7 +91,20 @@
                             @foreach($soal->bank_soal_jawaban as $key => $jwb)
                             <div class="flex items-center mt-4 mb-2">
                                 <input id="jwb_{{ $jwb->id }}" {{ $key == 0 ? "required" : "" }} type="radio" value="{{ $jwb->id }}" name="jawaban[{{ $jwb->bank_soal_id }}]" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                <label for="jwb_{{ $jwb->id }}" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">{{ ucfirst($jwb->jawaban) }}</label>
+                                <label for="jwb_{{ $jwb->id }}" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
+                                    @switch($key)
+                                        @case(0)
+                                            A.
+                                            @break
+                                        @case(1)
+                                            B.
+                                            @break
+                                        @case(2)
+                                            C.
+                                            @break
+                                    @endswitch
+                                    {{ ucfirst($jwb->jawaban) }}
+                                </label>
                             </div>
                             @endforeach
                         </div>
--- resources/views/panel/dashboard.blade.php
+++ resources/views/panel/dashboard.blade.php
@@ -121,7 +121,7 @@
             </div>
             <div class="mt-3">
                 <button type="button" id="refresh_dt_kuesioner" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Filter</button>
-                <a href="#" id="export_data" target="_blank" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Export</a>
+                <a href="#" id="export_data" target="_blank" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Export CSV</a>
             </div>
           </div>
         </div>
