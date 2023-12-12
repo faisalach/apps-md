ea3d0557b20d9410151175e0c6356a43a503fd2b
faisalach <faisalachramsyah@gmail.com>
2023-12-12 07:38:05 +0700

    update form

--- resources/views/kuesioner/form.blade.php
+++ resources/views/kuesioner/form.blade.php
@@ -34,15 +34,15 @@
                     <div class="bg-white rounded-lg py-3 px-5 mb-3">
                         <label class="text-xs uppercase font-semibold">Jenis Kelamin*</label>
                         <div class="flex items-center mt-4 mb-2">
-                            <input id="laki-laki" required type="radio" value="laki-laki" name="jenis_kelamin" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="laki-laki" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">Laki-laki</label>
+                            <input id="laki-laki" required type="radio" value="laki-laki" name="jenis_kelamin" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                            <label for="laki-laki" class="ms-2 text-lg font-medium text-gray-900">Laki-laki</label>
                         </div>
                         <div class="flex items-center mb-2">
-                            <input id="perempuan" type="radio" value="perempuan" name="jenis_kelamin" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="perempuan" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">Perempuan</label>
+                            <input id="perempuan" type="radio" value="perempuan" name="jenis_kelamin" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                            <label for="perempuan" class="ms-2 text-lg font-medium text-gray-900">Perempuan</label>
                         </div>
                     </div>
-                    <div class="grid grid-cols-2 gap-3">
+                    <div class="grid md:grid-cols-2 gap-3">
                         <div class="bg-white rounded-lg py-3 px-5 mb-3">
                             <label for="tempat_lahir" class="text-xs uppercase font-semibold">Tempat Lahir*</label>
                             <input type="text" required id="tempat_lahir" name="tempat_lahir" class="px-0 block border-0 w-full hover:border-0 focus:ring-0 leading-tight text-lg" placeholder="Tempat Lahir">
@@ -55,20 +55,20 @@
                     <div class="bg-white rounded-lg py-3 px-5 mb-3">
                         <label class="text-xs uppercase font-semibold">Golongan Darah</label>
                         <div class="flex items-center mt-4 mb-2">
-                            <input id="gol_a" type="radio" value="A" name="golongan_darah" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="gol_a" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">A</label>
+                            <input id="gol_a" type="radio" value="A" name="golongan_darah" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                            <label for="gol_a" class="ms-2 text-lg font-medium text-gray-900">A</label>
                         </div>
                         <div class="flex items-center mb-2">
-                            <input id="gol_b" type="radio" value="B" name="golongan_darah" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="gol_b" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">B</label>
+                            <input id="gol_b" type="radio" value="B" name="golongan_darah" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                            <label for="gol_b" class="ms-2 text-lg font-medium text-gray-900">B</label>
                         </div>
                         <div class="flex items-center mb-2">
-                            <input id="gol_ab" type="radio" value="AB" name="golongan_darah" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="gol_ab" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">AB</label>
+                            <input id="gol_ab" type="radio" value="AB" name="golongan_darah" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                            <label for="gol_ab" class="ms-2 text-lg font-medium text-gray-900">AB</label>
                         </div>
                         <div class="flex items-center mb-2">
-                            <input id="gol_o" type="radio" value="O" name="golongan_darah" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                            <label for="gol_o" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">O</label>
+                            <input id="gol_o" type="radio" value="O" name="golongan_darah" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                            <label for="gol_o" class="ms-2 text-lg font-medium text-gray-900">O</label>
                         </div>
                     </div>
 
@@ -100,8 +100,8 @@
                         <div class="mt-4">
                             @foreach($soal->bank_soal_jawaban as $key => $jwb)
                             <div class="flex items-center mt-4 mb-2">
-                                <input id="jwb_{{ $jwb->id }}" {{ $key == 0 ? "required" : "" }} type="radio" value="{{ $jwb->id }}" name="jawaban[{{ $jwb->bank_soal_id }}]" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                <label for="jwb_{{ $jwb->id }}" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">
+                                <input id="jwb_{{ $jwb->id }}" {{ $key == 0 ? "required" : "" }} type="radio" value="{{ $jwb->id }}" name="jawaban[{{ $jwb->bank_soal_id }}]" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500">
+                                <label for="jwb_{{ $jwb->id }}" class="ms-2 text-lg font-medium text-gray-900">
                                     @switch($key)
                                         @case(0)
                                             A.
@@ -121,7 +121,7 @@
                     </div>
                     @endforeach
                 </div>
-                <button type="submit" class=" w-full focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Kirim</button>
+                <button type="submit" class=" w-full focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2">Kirim</button>
             </div>
         </form>
     </div>
