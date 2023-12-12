ca4e6ff70fb82b22eb29f49af4761e62688e6ecd
faisalach <faisalachramsyah@gmail.com>
2023-12-12 20:19:39 +0700

    update login

--- resources/views/panel/login.blade.php
+++ resources/views/panel/login.blade.php
@@ -1,40 +1,39 @@
 @include('layout.header')
-    <div style="background-image: url('/assets/bg1.jpg')" class="min-h-screen bg-repeat-y bg-cover p-4">
-        <form action="{{ route('authenticated') }}" method="POST">
+    <div style="background-image: url('/assets/bg2.jpg')" class="min-h-screen relative flex items-center justify-center bg-repeat-y bg-cover p-4">
+        <span class="fixed top-0 left-0 bottom-0 right-0 bg-gray-500 z-5 opacity-5"></span>
+        <form action="{{ route('authenticated') }}" method="POST" class="relative z-10">
             @csrf
-            <div class="w-md bg-[#AAD2FF] p-3 rounded-xl max-w-full w-[920px] m-auto px-8 py-4">
-                <div class="text-center mb-10">
-                    <h1 class="text-4xl leading-tight font-bold">Form Test</h1>
-                    <h1 class="text-4xl leading-tight font-bold"><span class="text-[#008000]">Apps</span><span class="text-white">MD</span> <span class="text-[#0000ff]">& Gaya Belajar</span></h1>
-                    <h3 class="text-xl leading-tight mt-2 font-semibold italic ">Aplikasi Mengenal Diri</h3>
+            <div class="w-96 bg-white shadow-lg border border-gray-100 p-3 rounded-xl max-w-full  m-auto px-8 py-4">
+                <div class="text-center pb-5 mb-5 border-b">
+                    <h3 class="text-xl leading-tight mt-10 font-semibold ">Aplikasi Mengenal Diri</h3>
+                    <h5 class="font-semibold mb-3 mt-4 text-gray-500">Log In</h5>
                 </div>
                 @if (Session::has('message'))
-                <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                <div class="p-4 mb-4 text-sm text-yellow-800 rounded-lg bg-yellow-50" role="alert">
                     {{ Session::get("message") }}
                 </div>
                 @endif
-                <div class="mb-10">
-                    <h5 class="font-semibold ml-6 mb-3"><span class="text-lg">LOG IN</span></h5>
-                    <div class="bg-white rounded-lg py-3 px-5 mb-3">
+                <div class="mb-5 pb-5 border-b border-gray-100">
+                    <div class="mb-3">
                         <label for="username" class="text-xs uppercase font-semibold">Username*</label>
-                        <input type="text" value="{{ old("username") }}" required id="username" name="username" class="px-0 block border-0 w-full hover:border-0 focus:ring-0 leading-tight text-lg" placeholder="Masukkan Username">
+                        <input type="text" value="{{ old("username") }}" required id="username" name="username" class="border-b border-gray-200 px-0 block border-0 w-full focus:ring-0 leading-tight bg-transparent border-bottom" placeholder="Masukkan Username">
                         @error("username")
                         <p class="text-xs p-0 m-0 text-red-500">* {{ $errors->first('username') }}</p>
                         @enderror
                     </div>
-                    <div class="bg-white rounded-lg py-3 px-5 mb-3">
+                    <div class="mb-3">
                         <label for="password" class="text-xs uppercase font-semibold">Password*</label>
-                        <input type="password" required id="password" name="password" class="px-0 block border-0 w-full hover:border-0 focus:ring-0 leading-tight text-lg" placeholder="Masukkan Password">
+                        <input type="password" required id="password" name="password" class="border-b border-gray-200 px-0 block border-0 w-full focus:ring-0 leading-tight bg-transparent border-bottom" placeholder="Masukkan Password">
                         @error("password")
                         <p class="text-xs p-0 m-0 text-red-500">* {{ $errors->first('password') }}</p>
                         @enderror
                     </div>
                     <div class="flex items-center mt-4 mb-2">
-                        <input id="remember_me" type="checkbox" {{ old('remember_me') ? 'checked' : '' }} value="1" name="remember_me" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="remember_me" class="ms-2 text-lg font-medium text-gray-900 dark:text-gray-300">Remember Me</label>
+                        <input id="remember_me" type="checkbox" {{ old('remember_me') ? 'checked' : '' }} value="1" name="remember_me" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500">
+                        <label for="remember_me" class="ms-2 font-medium text-sm text-gray-900 dark:text-gray-300">Remember Me</label>
                     </div>
                 </div>
-                <button type="submit" class=" w-full focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Kirim</button>
+                <button type="submit" class=" w-full focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2">Kirim</button>
             </div>
         </form>
     </div>
